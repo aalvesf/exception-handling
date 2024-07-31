31d11bbad5cf90a622448d714a756cb5a556e3a0
Cevdet Çağdaş Köse <chatonode@gmail.com>
2024-07-31 20:49:44 +0100

    feat: Complete Exercise 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,20 @@
 import exercises.Exercise1;
 import exercises.Exercise2;
+import exercises.Exercise3;
 
 public class Main {
 
     public static void main(String[] args) {
+        /** Exercise 1 */
 //        System.out.println(Exercise1.divide(10, 2));
 //        System.out.println(Exercise1.divide(10, 0));
 
-        int[] inputs;
-        inputs = Exercise2.getUserInput();
-        System.out.println(Exercise1.divide(inputs[0], inputs[1]));
+        /** Exercise 2 */
+//        int[] inputs;
+//        inputs = Exercise2.getUserInput();
+//        System.out.println(Exercise1.divide(inputs[0], inputs[1]));
 
+        /** Exercise 2 */
+        Exercise3.divide();
     }
 }
